0a2d0c40bef844fa2edb259a1b9c98b44df8bd00
gridbrown <59910838+gridbrown@users.noreply.github.com>
2020-08-27 12:16:12 +0900

    0827

--- PSD_17_Waxom_Free_PSD_Template/site/js/script.js
+++ PSD_17_Waxom_Free_PSD_Template/site/js/script.js
@@ -1,8 +1,17 @@
-$(function() {
-	
+$(function () {
+    toggle = $('.toggle');
+    menu = $('.menu');
+    toggle.click(function () {
+        menu.slideToggle();
+    })
 
-	
+
+    $(window).resize(function () {
+        if ($(this).whidth() > 768) {
+            menu.show();
+        } else {
+            menu.hide();
+        }
+    })
 
 });
-
-
